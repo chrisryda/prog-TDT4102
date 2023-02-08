4a85904839dd7e56e50c7a5c2ac76dc812a581cf
chrisryda <chrisryda@hotmail.com>
2023-02-08 22:36:00 +0100

    Refactoring

--- oving4/main.cpp
+++ oving4/main.cpp
@@ -5,54 +5,15 @@
 using namespace std;
 
 int main() {
-    int choice = 100;
-    while(choice != 0) {
-        cout << "\nChoose action:\n"
-             << "0) Exit\n"
-             << "1) Test callByValue()\n"
-             << "2) Test callByReference()\n"
-             << "3) Test swapNumbers()\n"
-             << "4) Test printStudent()\n"
-             << "5) Test isInProgram()\n"
-             << "6) Test testString()\n"
-             << "7) Test readInputToString()\n"
-             << "8) Test countChar()\n"
-             << "9) Test playMastermind()()\n"
+    testCallByValue();
+    testCallByReference();
+    testSwapNumbers();
+    testPrintStudent();
+    testIsInProgram();
+    testString();
+    testReadInputToString();
+    testCountChar();
+    playMastermind(5, false);
 
-             << "Choose option (0-9): ";
-        cin >> choice;
-        cout << "\n\n" << endl;
-        switch (choice) {
-            case 1:
-                testCallByValue();
-                break;
-            case 2:
-                testCallByReference();
-                break;
-            case 3:
-                testSwapNumbers();
-                break;
-            case 4:
-                testPrintStudent();
-                break;
-            case 5:
-                testIsInProgram();
-                break;
-            case 6:
-                testString();
-                break;
-            case 7:
-                testReadInputToString();
-                break;
-            case 8:
-                testCountChar();
-                break;
-            case 9:
-                playMastermind(5);
-                break;
-            default:
-                break;
-        }
-    }
     return 0;
 }
--- oving4/mastermind.h
+++ oving4/mastermind.h
@@ -2,6 +2,6 @@
 
 #include <string>
 
-void playMastermind(int n);
+void playMastermind(int n, bool hide_code);
 int checkCharactersAndPosition(std::string code, std::string guess);
 int checkCharacters(std::string code, std::string guess);
