943c32cafbf4ced90d5e22587532caa3bc193eaa
chrisryda <chrisryda@hotmail.com>
2023-02-08 22:34:46 +0100

    Removed comment

--- oving4/tests.h
+++ oving4/tests.h
@@ -5,11 +5,6 @@ void testCallByReference();
 void testSwapNumbers();
 void testPrintStudent();
 void testIsInProgram();
-
-// Oppgave 2e): testStudentStruct forøker å redefinere struct Student?
-// Det er heller ingen grunn til å include utilities.h i denne filen, 
-// men den bør ikke gi redefinition feilmelding.
-
 void testString();
 void testReadInputToString();
 void testCountChar();
