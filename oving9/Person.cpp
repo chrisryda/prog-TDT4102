d092682933331cc2a4c96b56d57bc762f043d682
chrisryda <chrisryda@hotmail.com>
2023-03-21 17:56:43 +0100

    Completed task 1 and 2

---
+++ oving9/Person.cpp
@@ -0,0 +1,37 @@
+#include "Person.h"
+#include "Car.h"
+#include <string>
+#include <memory>
+#include <iostream>
+
+using namespace std;
+
+// Person::Person(string name, string email, Car* car = nullptr) : name{name}, email{email}, car{car} {}
+Person::Person(string name, string email, unique_ptr<Car> c) : name{name}, email{email} {
+    car = move(c);
+}
+
+string Person::getName() const {
+    return name;
+}
+
+string Person::getEmail() const {
+    return email;
+}
+
+void Person::setEmail(std::string newEmail) {
+    email = newEmail;
+}
+
+bool Person::hasAvailableSeats() const {
+    return (car.get() != nullptr && car.get()->hasFreeSeats());
+}
+
+ostream& operator<<(ostream &os, const Person &p) {
+    os << "\nPerson" << endl;
+    os << "Name: " << p.name << endl;
+    os << "Email: " << p.email << endl;
+    os << "Has available seats: " << p.hasAvailableSeats() << endl;
+
+    return os;
+}
---
+++ oving9/main.cpp
@@ -0,0 +1,19 @@
+#include "Car.h"
+#include "Person.h"
+#include <iostream>
+#include <memory>
+
+using namespace std;
+
+int main() {
+    Person p1{"Audust", "a@d.com"};
+    cout << p1 << endl;
+
+    Person p2{"Krisa", "c@d.com", make_unique<Car>(3)};
+    cout << p2 << endl;
+
+    Person p3{"T-bone", "t@b.com", make_unique<Car>(0)};
+    cout << p3 << endl;
+
+    return 0;
+}
