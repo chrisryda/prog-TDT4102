0fdb7298b34c2bf45fe75c00ebe225ad36b3e862
chrisryda <chrisryda@hotmail.com>
2023-03-29 22:07:04 +0200

    Completed task 2

--- animation/oving10/Tile.cpp
+++ animation/oving10/Tile.cpp
@@ -1,5 +1,6 @@
 #include "Tile.h"
 #include <map>
+#include <cassert>
 
 // For aa sette labelfarge i henhold til hvor mange miner som er rundt
 const std::map<int, TDT4102::Color> minesToColor {
@@ -24,10 +25,35 @@ Tile::Tile(TDT4102::Point pos, int size) : Button({pos.x, pos.y}, 1.5*size, size
 	setButtonColor(TDT4102::Color::silver);
 }
 
-void Tile::open()
-{
+void Tile::setIsMine(bool isMine) {
+	isMine = isMine;
 }
 
-void Tile::flag()
-{
+void Tile::open() {
+	if (state == Cell::open || state == Cell::flagged) {
+		return;
+	}
+	state = Cell::open;
+	set_label_color(TDT4102::Color::white);
+	if (isMine) {
+		set_label("X");
+		set_label_color(TDT4102::Color::red);
+	}
+}
+
+void Tile::flag() {
+	if (state == Cell::flagged) {
+		state = Cell::closed;
+		set_label(cellToSymbol.at(Cell::closed));
+	} else {
+		state = Cell::flagged;
+		set_label(cellToSymbol.at(Cell::flagged));
+		set_label_color(TDT4102::Color::black);
+	}
+}
+
+void Tile::setAdjMines(int n) {
+	assert(n > 0 && n < 9);
+	set_label(std::to_string(n));
+	set_label_color(minesToColor.at(n));
 }
--- animation/oving10/Tile.h
+++ animation/oving10/Tile.h
@@ -10,11 +10,15 @@ class Tile : public TDT4102::Button
 	Cell state = Cell::closed;
 	void set_label(std::string s) { this->setLabel(s);}
 	void set_label_color(TDT4102::Color c) {this->setLabelColor(c);}
+	bool isMine = false;
 public:
 	Tile(TDT4102::Point pos, int size);
 
+	Cell getState() const { return state; };
+	bool getIsMine() const { return isMine; };
+	void setIsMine(bool isMine);
+
 	void open();
 	void flag();
-
-	Cell getState() const { return state; };
+	void setAdjMines(int n);
 };
