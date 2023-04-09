ae949e0b540af4a2880ec60fec8e4c52a6fbeefe
chrisryda <chrisryda@hotmail.com>
2023-04-09 20:05:57 +0200

    Made Node and LinkedList template classes

--- oving11/3/LinkedList.cpp
+++ oving11/3/LinkedList.cpp
@@ -1,71 +1,13 @@
 #include "LinkedList.h"
 
-LinkedList::Node* LinkedList::LinkedList::insert(Node *pos, const std::string& value) {
-    std::unique_ptr<Node> newNode;
-    
-    if (pos == head.get() && !isEmpty()) {
-        std::cerr << "Terminating: Cannot add node before head" << std::endl;
-        throw new std::invalid_argument("Cannot add node before head");
-    } else if (isEmpty()) {
-        newNode = std::make_unique<Node>(value, nullptr, head.get());
-        Node* ret = newNode.get();
-
-        head->next = std::move(newNode);
-        return ret;
-    } else {
-        newNode = std::make_unique<Node>(value, nullptr, nullptr);
-        Node* ret = newNode.get();
-        
-        newNode->prev = pos->prev;
-        newNode->next = std::move(pos->prev->next);
-        pos->prev = newNode.get();
-        newNode->prev->next = std::move(newNode);
-        return ret;
-    }
-    return newNode.get();
-}
-
-LinkedList::Node* LinkedList::LinkedList::find(const std::string& value) {
-    Node* n = head.get();
-    while (n != nullptr) {
-        if ((n->getValue()) == value) {
-            return n;
-        }
-        n = n->getNext();
-    }
-    return tail->getNext();
-}
-
-LinkedList::Node* LinkedList::LinkedList::remove(Node* pos) {
-    if (pos == head.get()) {
-        std::cerr << "Terminating: Cannot remove head/tail" << std::endl;
-        throw new std::invalid_argument("Cannot remove head/tail");
-    } else {
-        Node* ret = pos->getNext(); 
-        pos->next->prev = pos->prev;
-        pos->prev->next = std::move(pos->next);
-        return ret;
-    }
-}
-
-void LinkedList::LinkedList::remove(const std::string& value) {
-    Node* n = head.get();
-    while (n != nullptr) {
-        if (n->getValue() == value) {
-            remove(n);
-        }
-        n = n->getNext();
-    }
-}
-
 void testLinkedList() {
-    LinkedList::LinkedList list;
-    LinkedList::Node* first = list.insert(list.end(), "first");
-    LinkedList::Node* second = list.insert(first, "second");
+    LinkedList::LinkedList<std::string> list;
+    LinkedList::Node<std::string>* first = list.insert(list.end(), "first");
+    LinkedList::Node<std::string>* second = list.insert(first, "second");
     list.insert(second, "third");
     
-    LinkedList::Node* secondFound = list.find("second");
-    LinkedList::Node* found = list.insert(secondFound, "found");
+    LinkedList::Node<std::string>* secondFound = list.find("second");
+    LinkedList::Node<std::string>* found = list.insert(secondFound, "found");
 
     std::cout << list << std::endl;
     list.remove(found);
--- oving11/3/LinkedList.h
+++ oving11/3/LinkedList.h
@@ -8,23 +8,27 @@
 void testLinkedList();
 
 namespace LinkedList {
+
+template <typename T>
+class LinkedList;
+
+template <typename T>
 class Node {
 private:
-    const std::string value;
-    std::unique_ptr<Node> next;
-    Node* prev;
+    const T value;
+    std::unique_ptr<Node<T>> next;
+    Node<T>* prev;
 
 public:
+    Node(const T value, std::unique_ptr<Node<T>> next, Node<T>* prev) : value(value), next(std::move(next)), prev(prev) {}
     Node() : value(), next(nullptr), prev(nullptr) {}
-    Node(const std::string& value, std::unique_ptr<Node> next, Node* prev) : value(value), next(std::move(next)), prev(prev) {}
     ~Node() = default;
 
-    std::string getValue() const { return value; }
-    Node* getNext() const { return next.get(); }
-    Node* getPrev() const { return prev; }
+    T getValue() const { return value; }
+    Node<T>* getNext() const { return next.get(); }
+    Node<T>* getPrev() const { return prev; }
 
-    friend class LinkedList;
-    friend std::ostream & operator<<(std::ostream & os, const Node & node) {
+    friend std::ostream & operator<<(std::ostream & os, const Node<T> & node) {
         if (node.getPrev() == nullptr) {
             os << "\n--Head--" << std::endl;
             os << "Next: " << node.getNext()->getValue() << std::endl;
@@ -45,46 +49,87 @@ public:
         
         return os;
     }
+    
+    friend class LinkedList<T>;
 };
 
+template <typename T>
 class LinkedList {
 private:
-    std::unique_ptr<Node> head;
-    Node* const tail;
+    std::unique_ptr<Node<T>> head;
+    Node<T>* const tail;
 public:
-    LinkedList() : head(std::make_unique<Node>("head", nullptr, nullptr)), tail(head.get()) {}
+    LinkedList() : head(std::make_unique<Node<T>>()), tail(head.get()) {}
     ~LinkedList() = default;
 
     bool isEmpty() const { return head->next == nullptr; }
-    Node* begin() const { return head.get(); }
-    Node* end() const { return tail; }
+    Node<T>* begin() const { return head.get(); }
+    Node<T>* end() const { return tail; }
 
-    // The insert function takes a pointer to node (pos) and a string (value). It creates a new
-    // node which contains value. The new node is inserted into the LinkedList BEFORE the
-    // node pointed to by pos. Returns a pointer to the new Node
-    Node* insert(Node *pos, const std::string& value);
+    Node<T>* insert(Node<T> *pos, const T value) {
+        std::unique_ptr<Node<T>> newNode;
+    
+        if (pos == head.get() && !isEmpty()) {
+            std::cerr << "Terminating: Cannot add node before head" << std::endl;
+            throw new std::invalid_argument("Cannot add node before head");
+        } else if (isEmpty()) {
+            newNode = std::make_unique<Node<T>>(value, nullptr, head.get());
+            Node<T>* ret = newNode.get();
 
-    // The find function traverses the linked list and returns a pointer to the first node
-    // that contains the value given.
-    // If the value isn't in the list, find returns a pointer to the dummy node at the end
-    // of the list.
-    Node* find(const std::string& value);
+            head->next = std::move(newNode);
+            return ret;
+        } else {
+            newNode = std::make_unique<Node<T>>(value, nullptr, nullptr);
+            Node<T>* ret = newNode.get();
+            
+            newNode->prev = pos->prev;
+            newNode->next = std::move(pos->prev->next);
+            pos->prev = newNode.get();
+            newNode->prev->next = std::move(newNode);
+            return ret;
+        }
+        return newNode.get();
+    }
 
-    // The remove function takes a pointer to a node, and removes the node from the list. The
-    // function returns a pointer to the element after the removed node.
-    Node* remove(Node* pos);
+    Node<T>* find(const T value) {
+        Node<T>* n = head.get();
+        while (n != nullptr) {
+            if ((n->getValue()) == value) {
+                return n;
+            }
+            n = n->getNext();
+        }
+        return tail->getNext();
+    }
 
-    // The remove function takes a string and removes the first node which contains the value.
-    void remove(const std::string& value);
+    Node<T>* remove(Node<T>* pos) {
+        if (pos == head.get()) {
+            std::cerr << "Terminating: Cannot remove head/tail" << std::endl;
+            throw new std::invalid_argument("Cannot remove head/tail");
+        } else {
+            Node<T>* ret = pos->getNext(); 
+            pos->next->prev = pos->prev;
+            pos->prev->next = std::move(pos->next);
+            return ret;
+        }
+    }
 
-    // write a string representation of the list to the ostream
-    //-------------------// TODO: Fix print function-------------------
-    friend std::ostream & operator<<(std::ostream & os, const LinkedList& list) {
+    void remove(const T value) {
+        Node<T>* n = head.get();
+        while (n != nullptr) {
+            if (n->getValue() == value) {
+                remove(n);
+            }
+            n = n->getNext();
+        }
+    }
+
+    friend std::ostream & operator<<(std::ostream & os, const LinkedList<T>& list) {
         if (list.isEmpty()) {
             os << "List is empty" << std::endl;
             return os;
         }
-        Node* n = list.begin();
+        Node<T>* n = list.begin();
         while (n->getNext() != nullptr) {
             os << *n << std::endl;
             n = n->getNext();
@@ -93,4 +138,5 @@ public:
         return os;
     }
 };
+
 }
