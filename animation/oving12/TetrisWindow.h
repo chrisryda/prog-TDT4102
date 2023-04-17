58d5874e0a1e900a773cf2142dad314f290e26ac
chrisryda <chrisryda@hotmail.com>
2023-04-17 14:12:20 +0200

    Project init

---
+++ animation/oving12/TetrisWindow.h
@@ -0,0 +1,13 @@
+#pragma once
+#include "AnimationWindow.h"
+#include "Tetromino.h"
+#include "widgets/TextInput.h"
+#include "widgets/Button.h"
+
+class TetrisWindow : public TDT4102::AnimationWindow {
+public:
+    TetrisWindow();
+    void run();
+private:
+    void handleInput();
+};
---
+++ animation/oving12/main.cpp
@@ -0,0 +1,11 @@
+#include "Tetromino.h"
+#include "TetrisWindow.h"
+#include <iostream>
+
+int main() {
+	/********************************************************/
+		//Opprett vindu her
+    /********************************************************/
+        //kall run på det her
+	return 0;
+}
