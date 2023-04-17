77bb289d0b78280a1d1a2f2ac689b04add8e97e1
chrisryda <chrisryda@hotmail.com>
2023-04-17 14:47:47 +0200

    Implemented contructors

--- animation/oving12/Tetromino.cpp
+++ animation/oving12/Tetromino.cpp
@@ -1,21 +1,32 @@
 #include "Tetromino.h"
 #include <map>
-    
+#include <iostream>
+
 const std::map<TetrominoType, std::vector<std::vector<int>>> initialMatrixMap {
     {
-        TetrominoType::J,
-            {
+        TetrominoType::J, {
             {0, 0, 0},
             {1, 1, 1},
             {0, 0, 1}
-            }
+        }
     }
 };
 
-Tetromino::Tetromino(TDT4102::Point startingPoint, TetrominoType tetType) : 
-topLeftCorner{startingPoint}
-{
+Tetromino::Tetromino() : topLeftCorner{TDT4102::Point{0,0}}, matrixSize{0} {};
 
+Tetromino::Tetromino(TDT4102::Point startingPoint, TetrominoType tetType) : topLeftCorner{startingPoint} {
+    std::vector<std::vector<int>> tetrominoVec = initialMatrixMap.at(tetType);
+    matrixSize = tetrominoVec[0].size();
+    
+    blockMatrix.resize(matrixSize, std::vector<TetrominoType>(matrixSize, TetrominoType::NONE));
+    for (int row = 0; row < matrixSize; row++) {
+        for (int col = 0; col < matrixSize; col++) {
+            if (tetrominoVec[row][col] == 1) {
+                blockMatrix[row][col] = tetType;
+            }
+        }
+    }
+    // std::cout << matrixSize << std::endl;
 }
 
 void Tetromino::rotateCounterClockwise() {
